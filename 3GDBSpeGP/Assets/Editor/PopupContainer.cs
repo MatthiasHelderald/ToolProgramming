e32022d38714fee57d500edacce67975bd1ce643
MatthiasHelderald <109383974+MatthiasHelderald@users.noreply.github.com>
2024-03-18 01:50:19 +0100

    Bon

--- 3GDBSpeGP/Assets/Editor/PopupContainer.cs
+++ 3GDBSpeGP/Assets/Editor/PopupContainer.cs
@@ -1,20 +1,44 @@
-using UnityEditor;
 using UnityEngine;
+using UnityEditor;
 
-namespace Editor
+public class PopupContainer : EditorWindow
 {
-    public class PopupContainer : EditorWindow
+    Rect button_yesRect;
+    Rect button_noRect;
+    
+    public Vector2 GetWindowSize()
     {
-        private static void ShowWindow()
+        return new Vector2(200, 150);
+    }
+    void OnGUI()
+    {
+        GUILayout.Label("Popup Options Example", EditorStyles.boldLabel);
         {
-            var window = GetWindow<PopupContainer>();
-            window.titleContent = new GUIContent("TITLE");
-            window.Show();
+            if (GUILayout.Button("button_yesRect", GUILayout.Width(100)))
+            {
+                //windowYes = true;
+            }
+            if (Event.current.type == EventType.Repaint) button_yesRect = GUILayoutUtility.GetLastRect();
         }
-
-        private void CreateGUI()
+        
+        GUILayout.Label("Popup Options Example", EditorStyles.boldLabel);
         {
-            
+            if (GUILayout.Button("button_yesRect", GUILayout.Width(100)))
+            {
+                //windowYes = false;
+            }
+            if (Event.current.type == EventType.Repaint) button_noRect = GUILayoutUtility.GetLastRect();
         }
     }
+
+    public void OnOpen()
+    {
+        Debug.Log("Popup opened: " + this);
+    }
+
+    public void OnClose()
+    {
+        Debug.Log("Popup closed: " + this);
+    }
+    
 }
