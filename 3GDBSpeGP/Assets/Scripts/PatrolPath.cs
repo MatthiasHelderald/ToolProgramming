c26deee7aa87b0d688609cf57de15930ee69c322
MatthiasHelderald <109383974+MatthiasHelderald@users.noreply.github.com>
2024-04-15 18:10:50 +0200

    ok

---
+++ 3GDBSpeGP/Assets/Scripts/PatrolPath.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PatrolPath : MonoBehaviour
+{
+    [SerializeField] private Vector3[] waypoints;
+
+    private void OnDrawGizmos()
+    {
+        if (waypoints == null || waypoints.Length <= 1)
+        {
+            return;
+        }
+            
+        Vector3 lastPos = waypoints[^1];
+        Handles.color = new Color(0.55f, 1f, 0.44f);
+        Vector3 tPos = transform.position;
+        Handles.DrawDottedLine(waypoints[0],tPos + lastPos,10f);
+            
+        for (var i = 0; i < waypoints.Length; i++)
+        {
+            Vector3 wPos = tPos + waypoints[i];
+            Handles.DrawWireDisc(wPos,Vector3.up ,0.5f);
+            Handles.PositionHandle(wPos, Quaternion.identity);
+            lastPos = waypoints[i];
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (waypoints == null || waypoints.Length <= 1)
+            return;
+
+        Vector3 tPos = transform.position;
+        for (var i = 0; i < waypoints.Length; i++)
+        {
+            Vector3 wPos = tPos + waypoints[i];
+            Handles.PositionHandle(wPos, Quaternion.identity);
+        }
+    }
+}
